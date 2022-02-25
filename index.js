b57f937be86976c0b197dc8e1e3d2aa47ec2d74d
sonia guamuch <sonia.guamuch@sarlatam.com>
2022-02-24 19:07:21 -0600

    Asunto Que paso? [ticket: X]

---
+++ index.js
@@ -0,0 +1,9 @@
+let paraghaphs = document.getElementsByTagName("p");
+console.log(paraghaphs.length);
+
+if(paraghaphs.length>0){
+
+    let paragraph =paraghaphs[0];
+    paragraph.innerText="Bienvenidos al bootcamp";
+}
+

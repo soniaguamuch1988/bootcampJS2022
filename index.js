865d4935fc8bf52607ae5e8734d2b9e333e06168
sonia guamuch <sonia.guamuch@sarlatam.com>
2022-02-24 19:16:43 -0600

    const 2 agregar cambios [ticket: 2]

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-let paraghaphs = document.getElementsByTagName("p");
-console.log(paraghaphs.length);
+const paraghaphs = document.getElementsByTagName("p");
+console.log("parrafos en el documento: "+paraghaphs.length);
 
 if(paraghaphs.length>0){
 
-    let paragraph =paraghaphs[0];
+    const paragraph =paraghaphs[0];
     paragraph.innerText="Bienvenidos al bootcamp";
 }
 
